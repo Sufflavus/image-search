d094ad261afe7a3c0cfc70fb891c0ffcd32acb2a
Tania <tatashat@gmail.com>
2016-10-05 20:33:45 +0000

    + api wrapper

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 This app is runned on Heroku: [https://tata-fcc-image-search.herokuapp.com/](https://tata-fcc-image-search.herokuapp.com/)
 
 ## List of used tecnologies and libraries:
-1. node.js
+1. [Node.js](https://nodejs.org/)
 1. [Express v 4.14.0](https://expressjs.com/)
 1. [MongoDB v 2.2.7](https://www.mongodb.com/)
--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/api/imagesearch/*', function (req, res) {
+    var searchRequest = req.params[0];
+    var offset = req.query.offset;
+    console.log(offset)
+    res.send('Hello World!');
+});
+
+app.get('/api/latest/imagesearch/', function (req, res) {
+    res.send('Hello World!');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
